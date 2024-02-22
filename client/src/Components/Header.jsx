1bb0bef47833aedd45048c71d71a4cec4e92f0bd
Suman-357 <4mh20cs098suman@gmail.com>
2024-02-22 15:51:32 +0530

    header search form functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,32 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {useSelector} from 'react-redux'
+import { useEffect, useState } from 'react';
 
 export default function Header() {
-    const {currentUser} = useSelector(state => state.user)
+    const {currentUser} = useSelector(state => state.user);
+    const [searchterm , setserchterm] = useState('');
+    const navigate = useNavigate();
+
+const handelsubmit = (e) => {
+    e.preventDefault();
+    const urlparams = new URLSearchParams(window.location.search);
+    urlparams.set('searchterm', searchterm);
+    const searchquery = urlparams.toString();
+    navigate(`/search?${searchquery}`);
+}
+
+useEffect(() => {
+    const urlparams = new URLSearchParams(location.search);
+    const searchtermfromurl = urlparams.get('searchterm');
+    if(searchtermfromurl){
+        setserchterm(searchtermfromurl);
+    }
+}, [location.search]);
+
+
+
+
   return (
    <header className='bg-slate-200 shadow-md'>
     <div className='flex justify-between item-center max-w-6xl mx-auto p-3'>
@@ -13,9 +36,11 @@ export default function Header() {
             <span className='text-slate-700'>Estate</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type='text' placeholder='Search...' className='bg-transperent focus:outline-none w-24 sm:w-64'/>
-        <FaSearch className='text-slate-600' />
+        <form onSubmit={handelsubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type='text' value={searchterm} onChange={(e) => setserchterm(e.target.value)} placeholder='Search...' className='bg-transperent focus:outline-none w-24 sm:w-64'/>
+            <button>
+                <FaSearch className='text-slate-600' />
+            </button>
         </form>
         <ul className='flex gap-4'>
             <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
